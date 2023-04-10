58e004f71df5929e82a4cc33e716bda5193787c5
Joelle LIndsey <joelle.lindsey@vogappdevelopers.com>
2023-04-10 08:38:02 -0600

    added base code

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state - { hasError: false };
+
+    }
+
+    static getDerivedStateFromError(error) {
+        //updated state so the next render will show the fallback UI
+        return { hasError: true }
+    }
+
+    componentDidCatch(error, info) {
+        logErrorToMyService(error.info.componentStack)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return this.props.children
+        }
+
+        return this.props.children;
+    }
+
+}
+
+export default ErrorBoundary;
